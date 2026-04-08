4b98eeb8f15f94e9a0119ee098b321774b0143ea
Shahbaz <shahbaz.mehar@outlook.com>
2026-04-08 12:43:07 +0500

    Remove the orderNo and replace it with params

--- cartridges/int_marketpay_headless/cartridge/controllers/MarketPay.js
+++ cartridges/int_marketpay_headless/cartridge/controllers/MarketPay.js
@@ -72,7 +72,10 @@ server.post('PaymentSuccess', server.middleware.https, function (req, res, next)
                 }
             }
 
-            marketPayRedirectHelpers.onSuccessRedirect(req, res, orderNo);
+            marketPayRedirectHelpers.onSuccessRedirect(req, res, {
+                OrderNo: orderNo,
+                UserLocale: order.custom.marketPayUserLocale
+            });
         } else {
             Logger.error('MarketPay - Payment failed - Order with ID: ' + orderNo + ' not found in SFCC!');
             throw new Error('Order with ID: ' + orderNo + ' not found in SFCC!');
@@ -80,7 +83,10 @@ server.post('PaymentSuccess', server.middleware.https, function (req, res, next)
 
     } catch (e) {
         Logger.error('MarketPay - Payment failed - General Error due to exception. Error message: ' + e.message);
-        marketPayRedirectHelpers.onFailtureRedirect(req, res, orderNo);
+        marketPayRedirectHelpers.onFailtureRedirect(req, res, {
+                OrderNo: orderNo,
+                UserLocale: order ? order.custom.marketPayUserLocale : marketPayDataHelper.getDefaultLocale() 
+            });
     }
 
     return next();
@@ -90,11 +96,13 @@ server.post('PaymentSuccess', server.middleware.https, function (req, res, next)
  * Controller for failed payments.
  */
 server.post('PaymentFail', server.middleware.https, function (req, res, next) {
+    const marketPayDataHelper = require('*/cartridge/scripts/helpers/marketPayDataHelper');
     const marketPayRedirectHelpers = require('*/cartridge/scripts/helpers/marketPayRedirectHelpers');
     const orderNo = req.form.shop_orderid;
+    var order = null;
 
     try {
-        const order = COHelpers.getOrder(orderNo);
+        order = COHelpers.getOrder(orderNo);
         if (!order) {
             Logger.error('MarketPay - PaymentFail - Order not found. OrderNo: ' + orderNo);
         } else if (order.getStatus().value !== dw.order.Order.ORDER_STATUS_FAILED) {
@@ -108,7 +116,10 @@ server.post('PaymentFail', server.middleware.https, function (req, res, next) {
         Logger.error('MarketPay - PaymentFail - General Error due to exception. Error message: ' + e.message);
     }
 
-    marketPayRedirectHelpers.onFailtureRedirect(req, res, orderNo);
+    marketPayRedirectHelpers.onFailtureRedirect(req, res, {
+        OrderNo: orderNo,
+        UserLocale: order ? order.custom.marketPayUserLocale : marketPayDataHelper.getDefaultLocale()
+    });
 
     return next();
 });
