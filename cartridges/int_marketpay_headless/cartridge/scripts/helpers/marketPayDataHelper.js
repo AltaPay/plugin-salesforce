b641d8fb045e19cf73ff95286a1c40f88198a55b
Shahbaz <shahbaz.mehar@outlook.com>
2026-04-17 18:13:29 +0500

    Update version and release notes

--- cartridges/int_marketpay_headless/cartridge/scripts/helpers/marketPayDataHelper.js
+++ cartridges/int_marketpay_headless/cartridge/scripts/helpers/marketPayDataHelper.js
@@ -252,7 +252,7 @@ function getSessionDataModel() {
             transactionInfo: {
                 ecomPlatform: "Salesforce",
                 ecomPluginName: "int_marketpay_headless",
-                ecomPluginVersion: "2.0.6"
+                ecomPluginVersion: "2.0.7"
             }
         },
         callbacks: {
